2bf449209336dd851362da24981625e6796b475d
Mizerski <mizerskimachado@hotmail.com>
2024-06-19 02:55:15 -0300

    fix -> bg color

--- src/screens/statistics/styles.ts
+++ src/screens/statistics/styles.ts
@@ -4,7 +4,7 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: themes.colors.green_light,
+    backgroundColor: themes.colors.green_dark,
   },
   content: {
     height: "100%",
